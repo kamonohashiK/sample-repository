325c0afe972b81449ab25d2a4fb6efd006bf04ab
kamonohashiK <kamonohashiy@gmail.com>
2023-04-30 20:30:03 +0900

    #18 AppEngine用にコードを書き換え

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println(Hello())
+	http.HandleFunc("/", indexHandler)
+
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
+}
+
+// indexHandler responds to requests with our greeting.
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, Hello())
 }
 
 func Hello() string {
